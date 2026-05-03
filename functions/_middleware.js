53284a70ee7787b7d93f15f6860ee51eae422ef4
rimsh001 <bss10@bk.ru>
2026-05-03 21:07:29 +0300

    Normalize public HTML domain and contacts

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,5 +1,19 @@
+const SITE_DOMAIN = "https://aateam.ru";
+const LEGACY_DOMAIN = "https://asset-team.pages.dev";
 const OLD_EMAIL = "bss10@bk.ru";
 const NEW_EMAIL = "info@aateam.ru";
+
+const HTML_REPLACEMENTS = [
+  [LEGACY_DOMAIN, SITE_DOMAIN],
+  [OLD_EMAIL, NEW_EMAIL],
+  [`<p class="kicker">SEO-услуга</p>`, ""],
+  [`<p class="kicker">SEO услуга</p>`, ""]
+];
+
+const applyHtmlFixes = (html) => HTML_REPLACEMENTS.reduce(
+  (currentHtml, [from, to]) => currentHtml.replaceAll(from, to),
+  html
+);
 
 export async function onRequest(context) {
   const response = await context.next();
@@ -10,7 +24,7 @@ export async function onRequest(context) {
   }
 
   const html = await response.text();
-  const updatedHtml = html.replaceAll(OLD_EMAIL, NEW_EMAIL);
+  const updatedHtml = applyHtmlFixes(html);
 
   const headers = new Headers(response.headers);
   headers.delete("content-length");
